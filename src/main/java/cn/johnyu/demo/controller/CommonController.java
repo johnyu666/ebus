cc480e6ab64d606e11887f87ff47d4c1be48b6ac
yuyinghan <johnyucn@163.com>
2020-05-15 16:21:55 +0800

    ddsf

---
+++ src/main/java/cn/johnyu/demo/controller/CommonController.java
@@ -0,0 +1,24 @@
+package cn.johnyu.demo.controller;
+
+import cn.johnyu.demo.pojo.Album;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CommonController {
+    @RequestMapping(value = "/albums")
+    public List<Album> findAllAlbums(){
+        List<Album> albums=new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Album a=new Album();
+            a.setId(i);
+            a.setName("album"+i);
+            albums.add(a);
+        }
+
+        return albums;
+    }
+}
